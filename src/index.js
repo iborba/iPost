732db910ad30134fba56b3104e629b108c556218
Israel Borba <israel.borba@gmail.com>
2022-07-18 10:46:31 -0300

    test page for ipost

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3131;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.redirect('/index')
+});
+app.get('/index', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
